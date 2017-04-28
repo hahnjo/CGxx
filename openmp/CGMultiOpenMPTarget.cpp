7882bcd6d480fa8fafb8a789aee8af5d31750958
Jonas Hahnfeld <Hahnfeld@itc.rwth-aachen.de>
2017-04-28 09:20:07 +0200

    Implement overlapped gather with OpenMP

--- openmp/CGMultiOpenMPTarget.cpp
+++ openmp/CGMultiOpenMPTarget.cpp
@@ -1,6 +1,12 @@
 #include <cassert>
 #include <memory>
 
+#ifdef __INTEL_COMPILER
+// For check that there are exactly two devices.
+#include <cstdlib>
+#include <iostream>
+#endif
+
 #include <omp.h>
 
 #include "../CG.h"
@@ -45,6 +51,7 @@ class CGMultiOpenMPTarget : public CG {
   }
 
   virtual int getNumberOfChunks() override { return getNumberOfDevices(); }
+  virtual bool supportsOverlappedGather() override { return true; }
 
   virtual void init(const char *matrixFile) override;
 
@@ -55,8 +62,10 @@ class CGMultiOpenMPTarget : public CG {
   virtual void cpy(Vector _dst, Vector _src) override;
 
   void matvecGatherXViaHost(floatType *x);
-  void matvecKernelCRS(floatType *x, floatType *y);
-  void matvecKernelELL(floatType *x, floatType *y);
+  template <bool roundup = false>
+  void matvecKernelCRS(MatrixDataCRS *matrices, floatType *x, floatType *y);
+  template <bool roundup = false>
+  void matvecKernelELL(MatrixDataELL *matrices, floatType *x, floatType *y);
 
   virtual void matvecKernel(Vector _x, Vector _y) override;
   virtual void axpyKernel(floatType a, Vector _x, Vector _y) override;
@@ -68,11 +77,22 @@ class CGMultiOpenMPTarget : public CG {
   virtual void applyPreconditionerKernel(Vector _x, Vector _y) override;
 
 public:
-  CGMultiOpenMPTarget() : CG(MatrixFormatCRS, PreconditionerJacobi) {}
+  CGMultiOpenMPTarget()
+      : CG(MatrixFormatCRS, PreconditionerJacobi,
+           /* overlappedGather= */ true) {}
 };
 
 void CGMultiOpenMPTarget::init(const char *matrixFile) {
   int devices = getNumberOfDevices();
+#ifdef __INTEL_COMPILER
+  // Until #6000161589 is fixed, we can't have an asynchronous offloading that
+  // depends on previous tasks! To work that around, we manually unroll a loop
+  // in matvecGatherXViaHost() and assume two devices.
+  if (devices != 2) {
+    std::cerr << "Only supports exactly two devices!" << std::endl;
+    std::exit(1);
+  }
+#endif
 
   // Init the devices with a simple target region.
   for (int d = 0; d < getNumberOfDevices(); d++) {
@@ -134,10 +154,20 @@ void CGMultiOpenMPTarget::doTransferTo() {
                                          map(to: x[0:N], k[offset:length])
     switch (matrixFormat) {
     case MatrixFormatCRS:
-      enterMatrixCRS(splitMatrixCRS->data[d], length);
+      if (!overlappedGather) {
+        enterMatrixCRS(splitMatrixCRS->data[d], length);
+      } else {
+        enterMatrixCRS(partitionedMatrixCRS->diag[d], length);
+        enterMatrixCRS(partitionedMatrixCRS->minor[d], length);
+      }
       break;
     case MatrixFormatELL:
-      enterMatrixELL(splitMatrixELL->data[d], length);
+      if (!overlappedGather) {
+        enterMatrixELL(splitMatrixELL->data[d], length);
+      } else {
+        enterMatrixELL(partitionedMatrixELL->diag[d], length);
+        enterMatrixELL(partitionedMatrixELL->minor[d], length);
+      }
       break;
     default:
       assert(0 && "Invalid matrix format!");
@@ -204,8 +234,8 @@ void CGMultiOpenMPTarget::doTransferFrom() {
     #pragma omp target update nowait from(x[offset:length]) depend(out: x[0:N])
     // ... and delete the whole array afterwards.
 #ifdef __INTEL_COMPILER
-    // Until #6000161589 is fixed, we can't have an asynchronous offloading
-    // that depends on previous tasks!
+    // Until #6000161589 is fixed, we can't have an asynchronous offloading that
+    // depends on previous tasks!
     #pragma omp taskwait
 #endif
     #pragma omp target exit data nowait map(release: x[0:N]) depend(in: x[0:N])
@@ -215,10 +245,20 @@ void CGMultiOpenMPTarget::doTransferFrom() {
                                         map(release: k[offset:length])
     switch (matrixFormat) {
     case MatrixFormatCRS:
-      exitMatrixCRS(splitMatrixCRS->data[d], length);
+      if (!overlappedGather) {
+        exitMatrixCRS(splitMatrixCRS->data[d], length);
+      } else {
+        exitMatrixCRS(partitionedMatrixCRS->diag[d], length);
+        exitMatrixCRS(partitionedMatrixCRS->minor[d], length);
+      }
       break;
     case MatrixFormatELL:
-      exitMatrixELL(splitMatrixELL->data[d], length);
+      if (!overlappedGather) {
+        exitMatrixELL(splitMatrixELL->data[d], length);
+      } else {
+        exitMatrixELL(partitionedMatrixELL->diag[d], length);
+        exitMatrixELL(partitionedMatrixELL->minor[d], length);
+      }
       break;
     default:
       assert(0 && "Invalid matrix format!");
@@ -271,10 +311,30 @@ void CGMultiOpenMPTarget::matvecGatherXViaHost(floatType *x) {
     int offset = workDistribution->offsets[d];
     int length = workDistribution->lengths[d];
 
-    #pragma omp target update nowait device(d) from(x[offset:length])
+    #pragma omp target update nowait device(d) from(x[offset:length]) \
+                                     depend(out: x[offset:length])
   }
-  #pragma omp taskwait
 
+#ifdef __INTEL_COMPILER
+  // Until #6000161589 is fixed, we can't have an asynchronous offloading that
+  // depends on previous tasks! We have checked in init() that there are exactly
+  // two devices...
+  int d0 = 0;
+  int src0 = 1;
+  int offset0 = workDistribution->offsets[src0];
+  int length0 = workDistribution->lengths[src0];
+
+  #pragma omp target update nowait device(d0) to(x[offset0:length0]) \
+                                   depend(in: x[offset0:length0])
+
+  int d1 = 1;
+  int src1 = 0;
+  int offset1 = workDistribution->offsets[src1];
+  int length1 = workDistribution->lengths[src1];
+
+  #pragma omp target update nowait device(d1) to(x[offset1:length1]) \
+                                   depend(in: x[offset1:length1])
+#else
   // Transfer x to devices.
   for (int d = 0; d < getNumberOfDevices(); d++) {
     for (int src = 0; src < getNumberOfDevices(); src++) {
@@ -285,83 +345,132 @@ void CGMultiOpenMPTarget::matvecGatherXViaHost(floatType *x) {
       int offset = workDistribution->offsets[src];
       int length = workDistribution->lengths[src];
 
-      #pragma omp target update nowait device(d) to(x[offset:length])
+      #pragma omp target update nowait device(d) to(x[offset:length]) \
+                                       depend(in: x[offset:length])
     }
   }
+#endif
+
+  // In case of overlappedGather, we also wait for the first part of the
+  // computation to finish!
   #pragma omp taskwait
 }
 
-void CGMultiOpenMPTarget::matvecKernelCRS(floatType *x, floatType *y) {
+template <bool roundup>
+void CGMultiOpenMPTarget::matvecKernelCRS(MatrixDataCRS *matrices, floatType *x,
+                                          floatType *y) {
   int N = this->N;
 
   for (int d = 0; d < getNumberOfDevices(); d++) {
     int offset = workDistribution->offsets[d];
     int length = workDistribution->lengths[d];
 
-    int *ptr = splitMatrixCRS->data[d].ptr;
+    int *ptr = matrices[d].ptr;
     int nz = ptr[length];
-    int *index = splitMatrixCRS->data[d].index;
-    floatType *value = splitMatrixCRS->data[d].value;
+    int *index = matrices[d].index;
+    floatType *value = matrices[d].value;
 
 #pragma omp target nowait device(d) map(x[0:N], y[offset:length]) \
                    map(ptr[0:length+1], index[0:nz], value[0:nz])
 #pragma omp teams distribute parallel for
     for (int i = 0; i < length; i++) {
-      floatType tmp = 0;
-      #pragma omp simd reduction(+:tmp)
-      for (int j = ptr[i]; j < ptr[i + 1]; j++) {
-        tmp += value[j] * x[index[j]];
+      // Skip load and store if nothing to be done...
+      if (!roundup || ptr[i] != ptr[i + 1]) {
+        floatType tmp = (roundup ? y[offset + i] : 0);
+        #pragma omp simd reduction(+:tmp)
+        for (int j = ptr[i]; j < ptr[i + 1]; j++) {
+          tmp += value[j] * x[index[j]];
+        }
+        y[offset + i] = tmp;
       }
-      y[offset + i] = tmp;
     }
   }
 
-  #pragma omp taskwait
+  if (!overlappedGather) {
+    #pragma omp taskwait
+  }
 }
 
-void CGMultiOpenMPTarget::matvecKernelELL(floatType *x, floatType *y) {
+template <bool roundup>
+void CGMultiOpenMPTarget::matvecKernelELL(MatrixDataELL *matrices, floatType *x,
+                                          floatType *y) {
   int N = this->N;
 
   for (int d = 0; d < getNumberOfDevices(); d++) {
     int offset = workDistribution->offsets[d];
     int length = workDistribution->lengths[d];
 
-    int elements = splitMatrixELL->data[d].elements;
-    int *lengthA = splitMatrixELL->data[d].length;
-    int *index = splitMatrixELL->data[d].index;
-    floatType *data = splitMatrixELL->data[d].data;
+    int elements = matrices[d].elements;
+    int *lengthA = matrices[d].length;
+    int *index = matrices[d].index;
+    floatType *data = matrices[d].data;
 
 #pragma omp target nowait device(d) map(x[0:N], y[offset:length]) \
                    map(lengthA[0:N], index[0:elements], data[0:elements])
 #pragma omp teams distribute parallel for simd
     for (int i = 0; i < N; i++) {
-      floatType tmp = 0;
-      for (int j = 0; j < lengthA[i]; j++) {
-        int k = j * N + i;
-        tmp += data[k] * x[index[k]];
+      // Skip load and store if nothing to be done...
+      if (!roundup || lengthA[i] > 0) {
+        floatType tmp = (roundup ? y[offset + i] : 0);
+        for (int j = 0; j < lengthA[i]; j++) {
+          int k = j * N + i;
+          tmp += data[k] * x[index[k]];
+        }
+        y[offset + i] = tmp;
       }
-      y[offset + i] = tmp;
     }
   }
 
-  #pragma omp taskwait
+  if (!overlappedGather) {
+    #pragma omp taskwait
+  }
 }
 
 void CGMultiOpenMPTarget::matvecKernel(Vector _x, Vector _y) {
   floatType *x = getVector(_x);
   floatType *y = getVector(_y);
 
+  if (overlappedGather) {
+    // Start computation on the diagonal that does not require data exchange
+    // between the devices. It is efficient to do so before the gather because
+    // the computation is expected to take longer. This effectively even hides
+    // the overhead of starting the gather.
+    switch (matrixFormat) {
+    case MatrixFormatCRS:
+      matvecKernelCRS(partitionedMatrixCRS->diag.get(), x, y);
+      break;
+    case MatrixFormatELL:
+      matvecKernelELL(partitionedMatrixELL->diag.get(), x, y);
+      break;
+    default:
+      assert(0 && "Invalid matrix format!");
+    }
+  }
+
   matvecGatherXViaHost(x);
 
   switch (matrixFormat) {
   case MatrixFormatCRS:
-    matvecKernelCRS(x, y);
+    if (!overlappedGather) {
+      matvecKernelCRS(splitMatrixCRS->data.get(), x, y);
+    } else {
+      matvecKernelCRS<true>(partitionedMatrixCRS->minor.get(), x, y);
+    }
     break;
   case MatrixFormatELL:
-    matvecKernelELL(x, y);
+    if (!overlappedGather) {
+      matvecKernelELL(splitMatrixELL->data.get(), x, y);
+    } else {
+      matvecKernelELL<true>(partitionedMatrixELL->minor.get(), x, y);
+    }
     break;
   default:
     assert(0 && "Invalid matrix format!");
+  }
+
+  if (overlappedGather) {
+    // Wait for all tasks - this was skipped before!
+    #pragma omp taskwait
   }
 }
 
